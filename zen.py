ab61488d5486ffcdfbf4e79981ce69c2743a1a97
siddht4 <dhar.75.ap@gmail.com>
2019-11-08 14:41:30 +0530

    Update zen.py

--- zen.py
+++ zen.py
@@ -2,6 +2,9 @@ import zenmu
 print(zenmu)
 print(zenmu.zenmu)
 _a=b'\x01'
-print(zenmu.zenmu(_a))
-print(type(zenmu.zenmu(_a)))
+z=zenmu.zenmu(_a)
+print(z)
+print(type(z))
+for _i in z:
+  print(z[_i])
 
